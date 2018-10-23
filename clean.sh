b0ad5043c88398a1a168a8d6d6d527b85f2a9472
Mike Taylor <mtaylor@localhost.localdomain>
2018-10-23 14:45:41 -0700

    Initial

---
+++ clean.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rm -f -r CMakeFiles
+rm cmake_install.cmake
+rm CMakeCache.txt
+rm Makefile
+rm -f -r FOOBAR-prefix
+rm -f -r src
---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+extern int foo(void);
+
+int main(int argc, char **argv)
+{
+   foo();
+   return 0;
+}
+
